afb042163327bbac6bebee44ce4dbd6f50a08eb2
Joao Menegasso <joao.menegasso@sap.com>
2025-03-09 01:25:50 -0300

    Insert footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Code from './components/Code/Code'
 import './App.css'
 
 function App() {
-  const [content, setContent] = useState("https://joaolucasmm.github.io/portfolio");
+  const portfolio = "https://joaolucasmm.github.io/portfolio"
+
+  const [content, setContent] = useState(portfolio);
 
   return (
     <div className='app'>
-      <Insert setContent={setContent}/>
-      <Code genImage={content}/>
+      <Insert setContent={setContent} />
+      <Code genImage={content} />
+      <article className='footer'>made by <a href={portfolio}>João Lucas</a>. 2025</article>
     </div>
   )
 }
